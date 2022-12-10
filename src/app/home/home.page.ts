42cae70562512cf0d230e3e106caf43139306def
Nicolas <nicolas.barbosa@estudante.ufla.br>
2022-12-10 12:38:56 -0300

    Adicionando classificação ao IMC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,7 @@ export class HomePage {
   weight: number = 0
   height: number = 0
   imc: number = 0
+  msg: string = ""
 
   constructor(private toastCtrl: ToastController) {}
 
@@ -20,12 +21,19 @@ export class HomePage {
     }
     
     this.imc = this.weight / (this.height * this.height)
+
+    if (this.imc <= 18.5) { this.msg = "Magreza"}
+    else if (this.imc > 18.5 && this.imc <= 24.9) { this.msg = "Normal" }
+    else if (this.imc > 25 && this.imc <= 29.9) { this.msg = "Sobrepeso" }
+    else if (this.imc > 30 && this.imc <= 39.9) { this.msg = "Obesidade" }
+    else if (this.imc >= 40) { this.msg = "Obesidade grave" }
+
     this.showIMC()
   }
 
   async showIMC() {
     const toast = await this.toastCtrl.create({
-      message: `IMC = ${this.imc.toFixed(2)}`,
+      message: `IMC = ${this.imc.toFixed(2)} - ${this.msg}`,
       duration: 3000,
       color: 'secondary'
     })
